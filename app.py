d1074e1d1a30f8e2b676bf4e40229fe43d5bbd9e
Sam Pearson <sam.ed.pearson@gmail.com>
2024-11-29 12:19:09 -0600

    allowing app.py to be run from python instead of flask

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello World"
+
+#Allows starting the server by running this file from python instead of running flask
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# 7 minutes in
