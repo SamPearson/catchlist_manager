2d5c6a55da8e542fcbd215032d5e616e92ae2d3e
Sam Pearson <sam.ed.pearson@gmail.com>
2024-12-30 17:49:15 -0600

    styling - semantic ui

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def add():
     db.session.commit()
     return redirect(url_for("home"))
 
-app.route("/update/<int:todo_id>")
+@app.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
