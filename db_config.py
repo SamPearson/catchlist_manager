3f96492867281190376ffa4259d288dd180b3b5a
Sam Pearson <sam.ed.pearson@gmail.com>
2025-01-08 15:33:52 -0600

    using db directly instead of from the flask app parameter

--- db_config.py
+++ db_config.py
@@ -10,7 +10,7 @@ class Config:
 def initialize_database(app):
     """Check and initialize the database only if not already created."""
     with app.app_context():
-        if not inspect(app.extensions['sqlalchemy'].db.engine).has_table("todo"):
+        if not inspect(db.engine).has_table("todo"):
             print("Creating Database")
             app.extensions['sqlalchemy'].db.create_all()
             print("Database Created")
